1820ea6278f0151d10045850b336be2815de3161
Joshua Schniper <Joshua.Schniper@chiprewards.com>
2015-05-01 14:12:52 -0500

    Published 0.0.1

---
+++ node_tests/unit/lib/cloudfiles-test.js
@@ -0,0 +1,13 @@
+var chai           = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var MockUI         = require('ember-cli/tests/helpers/mock-ui');
+var CloudfilesAdapter      = require('../../../lib/cloudfiles');
+var chalk          = require('chalk');
+
+chai.use(chaiAsPromised);
+
+var CloudfilesAdapter;
+var expect = chai.expect;
+
+describe('CloudfilesAdapter', function() {
+});
